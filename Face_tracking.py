bb907830d282af37f2083de6c71908ef2a6eb38e
emmazingtv <emma@4richter.de>
2019-11-10 17:17:39 +0100

    yo wir wechsel zu pure data

--- Face_tracking.py
+++ Face_tracking.py
@@ -1,7 +1,33 @@
 import cv2
+import socket
 cap=cv2.VideoCapture(1)
 frameCount = 0
 #halloyo
+
+def send_Frequenz_and_Volume_to_pure_Data(x,y):
+    
+    s = socket.socket()
+    host = socket.gethostname()
+    port = 3000
+    s.connect((host, port))
+    maxFrequency = 800
+    minFrequency = 400
+    maxVolume = 100
+    width=1000
+    height=400 #einstellbar!!!!!!!
+    frequency = ((x/width)*(maxFrequency-minFrequency)+minFrequency)
+    volume=100-(10 ** (((y/height)*maxVolume)/50))-2
+    volume= int(volume)
+    if volume>100:
+        volume = 100
+    if volume<0:
+        volume = 0
+    message = "0 " + str(frequency) + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
+    s.send(message.encode('utf-8'))
+    message = "1 " + str(volume) + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
+    s.send(message.encode('utf-8'))
+    message = "2 " + str("12") + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
+    s.send(message.encode('utf-8'))
 
 while cap.isOpened():
     ret, frame = cap.read() 
@@ -40,6 +66,8 @@ while cap.isOpened():
                 #    cy=y
         #Schwerpunkt zeichen
         cv2.circle(frame,(cx, cy), 5, (0,0,255), cv2.FILLED)
+        send_Frequenz_and_Volume_to_pure_Data(cx+200,cy)
+        
     
 
     cv2.imshow("Video", frame) ##ohne seperation
@@ -51,3 +79,4 @@ while cap.isOpened():
     
 cap.release() 
 cv2.destroyAllWindows
+
---
+++ garbage/methods.py
@@ -0,0 +1,10 @@
+"""
+An entire file for you to expand. Add methods here, and the client should be
+able to call them with json-rpc without any editing to the pipeline.
+"""
+
+
+def count():
+    """It counts. Duh. Note: intentionally written to break on non-ints"""
+    frequenz = int(input("geb eine frequenz ein"))
+    return frequenz
