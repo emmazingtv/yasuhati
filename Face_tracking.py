3d963ab5afaa5f929e7db1fd283319940e726e99
emmazingtv <emma@4richter.de>
2019-11-11 21:55:24 +0100

    lol

--- Face_tracking.py
+++ Face_tracking.py
@@ -1,8 +1,9 @@
 import cv2
 import socket
+
 cap=cv2.VideoCapture(1)
 frameCount = 0
-#halloyo
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 def send_Frequenz_and_Volume_to_pure_Data(x,y):
     
@@ -13,8 +14,8 @@ def send_Frequenz_and_Volume_to_pure_Data(x,y):
     maxFrequency = 800
     minFrequency = 400
     maxVolume = 100
-    width=1000
-    height=400 #einstellbar!!!!!!!
+    width=600
+    height=300 #einstellbar!!!!!!!
     frequency = ((x/width)*(maxFrequency-minFrequency)+minFrequency)
     volume=100-(10 ** (((y/height)*maxVolume)/50))-2
     volume= int(volume)
@@ -34,45 +35,18 @@ while cap.isOpened():
     if ret == False: ## checkt is ein videobild da
         break
     #Frame in Graustufen wandeln
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
-    ## frame merken
-    if frameCount == 0: 
-        firstFrame = gray.copy() 
-    frameCount += 1
-
-    c = cv2.waitKey(30)
-    if 'n' == chr(c & 255):
-        firstFrame = gray.copy()
     
-    ##differenz bilden
-    output=cv2.absdiff(gray, firstFrame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-     ##schwellwertbildung (nur noch weiß und schwarz)
-    thresh = 40
-    ret, mask = cv2.threshold(output, thresh, 255, cv2.THRESH_BINARY) ##alle pixel unter dem threshhold wetden auf schwarz alle über aus weiß gesetzt
-
-    #Schwerpunkt berrechnen
-    M = cv2.moments(mask)
-    if M["m00"]:
-        cx = int(M["m10"]/M["m00"])
-        cy = int(M["m01"]/M["m00"]) ## ersetzen mit höchstem pixel (median)
-        #highestPointFound= False
-        #while highestPointFound ==False:
-           # for y in range(mask.shape[0]):
-            #    if mask[cx,y]==255: ##höchster pixel
-             #       if mask[cx,y+1] == 255 and mask[cx,y-1]==255 and mask[cx-1,y] ==255 and mask[cx+1,y]==255:##pixel drum rum checken#
-               #     highestPointFound=True
-                #    cy=y
-        #Schwerpunkt zeichen
-        cv2.circle(frame,(cx, cy), 5, (0,0,255), cv2.FILLED)
-        send_Frequenz_and_Volume_to_pure_Data(cx+200,cy)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
+    for (x,y,w,h) in faces:
+        cx=int(x)+int(w/2)
+        cv2.circle(frame,(cx, y), 5, (0,0,255), cv2.FILLED)
+        send_Frequenz_and_Volume_to_pure_Data(cx+200,y)
         
-    
-
     cv2.imshow("Video", frame) ##ohne seperation
-    cv2.imshow("Output", mask) ##mit seperation
     
+    c = cv2.waitKey(30)
     if ' ' == chr(c & 255):
         break
 
--- face_detection.py
+++ face_detection.py
@@ -13,15 +13,14 @@ while(cap.isOpened()):
 	if ret == False:
 		break
 
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 	for (x,y,w,h) in faces:
-		frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-		roi_gray = gray[y:y+h, x:x+w]
-		roi_color = frame[y:y+h, x:x+w]
-		eyes = eye_cascade.detectMultiScale(roi_gray)
-		for (ex,ey,ew,eh) in eyes:
-			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+		cx=int(x)+int(w/2)
+		cv2.circle(frame,(cx, y), 5, (0,0,255), cv2.FILLED)
+		
+		
+		
 
 
 	
