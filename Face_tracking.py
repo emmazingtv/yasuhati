11aa9c5172735fa13649f3791e76ee767bd655e2
emmazingtv <emma@4richter.de>
2019-11-16 15:18:32 +0100

    median

--- Face_tracking.py
+++ Face_tracking.py
@@ -4,6 +4,7 @@ import socket
 cap=cv2.VideoCapture(1)
 frameCount = 0
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+medianLength = 25
 
 def send_Frequenz_and_Volume_to_pure_Data(x,y):
     
@@ -30,19 +31,42 @@ def send_Frequenz_and_Volume_to_pure_Data(x,y):
     message = "2 " + str("12") + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
     s.send(message.encode('utf-8'))
 
+def calculateMedian(value, pastValues):
+    for i in range(0,len(pastValues)-1,1):
+        ##pastValues[4-i]=i
+        pastValues[len(pastValues)-1-i]=pastValues[len(pastValues)-2-i]
+    pastValues[0]=value
+    summe=0
+    for i in range(1, len(pastValues),1):
+        summe=summe+pastValues[i]
+    summe=summe/len(pastValues)
+    median=0.7*value+0.2*summe+0.05*pastValues[1]+0.05*pastValues[2]
+    return [median, pastValues]
+pastxValues=[0]*medianLength
+pastyValues=[0]*medianLength
 while cap.isOpened():
     ret, frame = cap.read() 
     if ret == False: ## checkt is ein videobild da
         break
     #Frame in Graustufen wandeln
-    
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+   
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
     for (x,y,w,h) in faces:
         cx=int(x)+int(w/2)
-        cv2.circle(frame,(cx, y), 5, (0,0,255), cv2.FILLED)
-        send_Frequenz_and_Volume_to_pure_Data(cx+200,y)
+        
+        X_list = calculateMedian(cx, pastxValues)
+        y_List = calculateMedian(y,pastyValues)
+        pastxValues = X_list[1]
+        pastyValues = y_List[1]
+        medianX = X_list[0]
+        medianY = y_List[0]
+        ##print(pastxValues, pastyValues)
+        cv2.circle(frame,(int(medianX), int(medianY)), 5, (0,0,255), cv2.FILLED)        
+        send_Frequenz_and_Volume_to_pure_Data(medianX+200,medianY)
         
     cv2.imshow("Video", frame) ##ohne seperation
     
