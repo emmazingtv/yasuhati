f2f154570b49ccb31714a0e3cbb31dc7a9df1084
emmazingtv <emma@4richter.de>
2019-12-07 17:57:54 +0100

    soundss

--- pyqt_test_with_threads.py
+++ pyqt_test_with_threads.py
@@ -15,7 +15,14 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 medianLength = 25
 pastxValues=[0]*medianLength
 pastyValues=[0]*medianLength
-
+currentMode=0
+currentVolume=0.5
+currentMinFrequency=400
+currentMaxFrequency=800
+currentDiySound1=17
+currentDiySound2=33
+currentDiySound3=17
+currentDiySound4=33
 
 def dark_palette():
 
@@ -49,28 +56,57 @@ def calculateMedian(value, pastValues):
     return [median, pastValues]
 
 def send_Frequenz_and_Volume_to_pure_Data(x,y,modus):
-    
+    global currentMinFrequency
+    global currentMaxFrequency
+    global currentVolume
+    global currentDiySound1
+    global currentDiySound2
+    global currentDiySound3
+    global currentDiySound4
+
     s = socket.socket()
     host = socket.gethostname()
     port = 3000
     s.connect((host, port))
-    maxFrequency = 800
-    minFrequency = 400
     maxVolume = 100
-    width=600
-    height=300 #einstellbar!!!!!!!
-    frequency = ((x/width)*(maxFrequency-minFrequency)+minFrequency)
-    volume=100-(10 ** (((y/height)*maxVolume)/50))-2
+    width=640
+    height=480 #einstellbar!!!!!!!
+    frequency = ((x/width)*(currentMaxFrequency-currentMinFrequency)+currentMinFrequency)
+    #volume = 100-(-1/187500*((y/height)*maxVolume)**3 + 27/2500*((y/height)*maxVolume)**2 - 2/75*((y/height)*maxVolume))
+    volume = 100 - (47/2275000000*((y/height)*maxVolume)**5 - 1507/1137500000*((y/height)*maxVolume)**4 - 5671/39000000*((y/height)*maxVolume)**3 + 33233/1820000*((y/height)*maxVolume)**2 - 6169/54600*((y/height)*maxVolume))
+    #volume=100-(10 ** (((y/height)*maxVolume)/50))-2
+    volume=volume*currentVolume
     volume= int(volume)
     if volume>100:
         volume = 100
     if volume<0:
         volume = 0
+
+
+    print(currentDiySound1)
+    diySoundGesamt = currentDiySound1+currentDiySound2+currentDiySound3+currentDiySound4
+    print(diySoundGesamt)
+    print(currentDiySound1)
+    DiySound1 = 100*currentDiySound1 / diySoundGesamt
+    print(currentDiySound1)
+    print(DiySound1)
+    DiySound2 = 100*currentDiySound2 / diySoundGesamt
+    DiySound3 = 100*currentDiySound3 / diySoundGesamt
+    DiySound4 = 100*currentDiySound4 / diySoundGesamt
+    
     message = "0 " + str(frequency) + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
     s.send(message.encode('utf-8'))
     message = "1 " + str(volume) + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
     s.send(message.encode('utf-8'))
     message = "2 " + str(modus) + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
+    s.send(message.encode('utf-8'))
+    message = "3 " + str(DiySound1) + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
+    s.send(message.encode('utf-8'))
+    message = "4 " + str(DiySound2) + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
+    s.send(message.encode('utf-8'))
+    message = "5 " + str(DiySound3) + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
+    s.send(message.encode('utf-8'))
+    message = "6 " + str(DiySound4) + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
     s.send(message.encode('utf-8'))
 
 class Worker(QRunnable):
@@ -80,10 +116,6 @@ class Worker(QRunnable):
     #changePixmap = pyqtSignal(QImage)
 
     
-    def __init__(self, *args, **kwargs):
-        super(Worker, self).__init__()
-        self.args = args
-        self.kwargs = kwargs
 
     @pyqtSlot()
     def run(self):
@@ -91,20 +123,24 @@ class Worker(QRunnable):
         global pastyValues
         global medianX
         global medianY
+        global currentMode
+        global currentVolume
         '''
         Your code goes in this function
         '''
         print("Thread start") 
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(1)
         while True:
-            args=self.args
-            kwargs=self.kwargs
-            print(args)
+           
+            
             ret, frame = cap.read()
             if ret == False: ## checkt is ein videobild da
                 break
             #Frame in Graustufen wandeln
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  
+            # height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            # print(width, height)
             
             faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
             for (x,y,w,h) in faces:
@@ -115,10 +151,9 @@ class Worker(QRunnable):
                 pastyValues = y_List[1]
                 medianX = X_list[0]
                 medianY = y_List[0]
-                print(medianX)
-                print(medianY)
+                
                 try:
-                    send_Frequenz_and_Volume_to_pure_Data(medianX+200,medianY,1)
+                    send_Frequenz_and_Volume_to_pure_Data(medianX+200,medianY,currentMode)
                 except ConnectionRefusedError:
                     print("nur für ein gesicht gedacht warte eine sekunde")##muss auch ins overlay
                     os.startfile("Zound_extended.pd") 
@@ -128,11 +163,13 @@ class Example(QWidget):
     
     def __init__(self):
         
-
+        
         super().__init__()
 
         self.threadpool = QThreadPool()
         print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
+        
+        
         
         app.setStyle('Fusion')
         app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
@@ -153,7 +190,6 @@ class Example(QWidget):
         
 
     def oh_no(self):
-        args=(1,2,3)
         worker = Worker()
         self.threadpool.start(worker) 
 
@@ -207,17 +243,25 @@ class Example(QWidget):
         combo.activated[str].connect(self.onActivatedCombo)  
 
     def onActivatedCombo(self, text):
-      
+        global currentMode
         #self.lbl.setText(text)
         #self.lbl.adjustSize() 
-        currentMode=text      #entweder returenen oder global variable oder noch ne funktion die die variablen ändert
+        modedict={
+            "Sound 1": 0,
+            "Sound 2": 1,
+            "Sound 3": 2,
+            "Schrei" : 3,
+
+        }
+        currentMode=modedict[text] 
+        print(currentMode)    #entweder returenen oder global variable oder noch ne funktion die die variablen ändert
         
 
     def Drehknopf(self):
         labeldial = QLabel("Volume", self)
 
         dial = QDial(self)
-        dial.setValue(30)
+        dial.setValue(50)
         dial.setGeometry(1540,160,350,350)
 
         labeldial.move(1600, 110)
@@ -227,7 +271,8 @@ class Example(QWidget):
         dial.valueChanged[int].connect(self.onActivatedDial) #he dial also emits sliderPressed() and sliderReleased() signals when the mouse button is pressed and released. Note that the dial’s value can change without these signals being emitted since the keyboard and wheel can also be used to change the value
 
     def onActivatedDial(self, number):
-        currentVolume=number
+        global currentVolume
+        currentVolume=number/100
 
     
     def Frequenzbereich(self):
@@ -238,14 +283,14 @@ class Example(QWidget):
         spinBox1.setRange(100, 5000)
         spinBox1.setSingleStep(100)
         spinBox1.setGeometry(1540,580,120,75)
-        spinBox1.setValue(100)
+        spinBox1.setValue(400)
         spinBox1.setFont(newfont)
 
         spinBox2 = QSpinBox(self)
         spinBox2.setRange(100, 5000)
         spinBox2.setSingleStep(100)
         spinBox2.setGeometry(1740,580,120,75)
-        spinBox2.setValue(1000)
+        spinBox2.setValue(800)
         spinBox2.setFont(newfont)
 
         labelfrequenz.move(1510, 500)
@@ -256,8 +301,10 @@ class Example(QWidget):
         spinBox2.valueChanged[int].connect(self.onActivatedSpinBox2)
 
     def onActivatedSpinBox1(self, nummer):
+        global currentMinFrequency
         currentMinFrequency=nummer
     def onActivatedSpinBox2(self, nummer):
+        global currentMaxFrequency
         currentMaxFrequency=nummer
 
 
@@ -277,14 +324,31 @@ class Example(QWidget):
         slider3.setValue(60)
         slider3.setGeometry(15,500,450,30)
 
+        slider4 = QSlider(Qt.Horizontal, self)
+        slider4.setValue(60)
+        slider4.setGeometry(15,570,450,30)
+
         labelslider.move(15, 300)
         newfont2 = QFont("Times", 30, QFont.Bold) 
         labelslider.setFont(newfont2)
 
-        slider.valueChanged[int].connect(self.onActivatedSlider)
+        slider.valueChanged[int].connect(self.onActivatedSlider1)
+        slider2.valueChanged[int].connect(self.onActivatedSlider2)
+        slider3.valueChanged[int].connect(self.onActivatedSlider3)
+        slider4.valueChanged[int].connect(self.onActivatedSlider4)
 
-    def onActivatedSlider(self, nummer):
-        currentDiySound=nummer
+    def onActivatedSlider1(self, nummer):
+        global currentDiySound1
+        currentDiySound1=nummer
+    def onActivatedSlider2(self, nummer):
+        global currentDiySound2
+        currentDiySound2=nummer
+    def onActivatedSlider3(self, nummer):
+        global currentDiySound3
+        currentDiySound3=nummer
+    def onActivatedSlider4(self, nummer):
+        global currentDiySound4
+        currentDiySound4=nummer
 
         
     
